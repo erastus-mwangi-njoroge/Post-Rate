104983a3addb71c990666c49967e5c3843f76b77
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-25 11:02:17 +0300

    add profile to the models

--- postAndRate/models.py
+++ postAndRate/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField()
     link = models.CharField(max_length=255)
-    image = models.ImageField('photos')
+    image = models.ImageField('postedPhotos')
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -22,3 +22,12 @@ class Rating(models.Model):
 
     def __str__(self):
         return f'{self.user.username} {self.project.title} Rating'
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField('profilePicture')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
