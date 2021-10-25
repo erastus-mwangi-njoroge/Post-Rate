e5738f817062031a5d012eaa1faf99373e524d31
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-26 01:31:04 +0300

    final changes to deployement

--- moringaawwards/settings.py
+++ moringaawwards/settings.py
@@ -73,8 +73,6 @@ STATICFILES_DIRS = (
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2!g$p38ym0*wx@a@p^z-cef7w&(7$&tb2f0omy-q(scidmeokl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -132,14 +130,7 @@ WSGI_APPLICATION = 'moringaawwards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awards',
-        'USER': 'moringa',
-        'PASSWORD':'db1234'
-    }
-}
+
 
 
 # Password validation
