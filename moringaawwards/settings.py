10baf70418f8441f378515a52650dbd5ad5de414
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-10-28 21:46:58 +0300

    add the lologin redirection

--- moringaawwards/settings.py
+++ moringaawwards/settings.py
@@ -177,6 +177,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/'
+
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
 
